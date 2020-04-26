044525149e192cb1eb383910a3ede991e417517c
jtparks <parksju@oregonstate.edu>
2020-04-26 15:13:01 -0700

    fixed runtime issue and logic errors, properly working classifier

--- starter.py
+++ starter.py
@@ -4,14 +4,14 @@ import re
 import math
 import time
 
-start = time.time()
-
 # Function to make a 1D list of sentences into a 2D list of words
 def create_list(aList):
     bList = []
     for line in aList:
+        line = clean_text(line)
         bList.append(line.split(" "))
     return bList
+
 
 # Preproccessor for the vectorizer
 def clean_text(text):
@@ -33,9 +33,10 @@ def clean_text(text):
     translate_dict = dict((c, " ") for c in filters)
     translate_map = str.maketrans(translate_dict)
     text = text.translate(translate_map)
-
     return text
 
+
+# Function to create the vocab from the imdb_data
 def create_vocab(imdb_data):
     # this vectorizer will skip stop words
     vectorizer = CountVectorizer(
@@ -53,88 +54,98 @@ def create_vocab(imdb_data):
     return vocabulary
 
 
-
-
-# Importing the dataset and creating the three distinct datasets
-imdb_data  = pd.read_csv('IMDB.csv', delimiter=',')
-label_data = pd.read_csv('IMDB_labels.csv', delimiter=',') 
-
-# Need a better way to to_list these, since we need the sanitized version
-training_data = create_list(imdb_data[:30000].iloc[:, 0].tolist())
-valid_data    = create_list(imdb_data[30000:40001].iloc[:, 0].tolist())
-
-testing_data  = create_list(imdb_data[40001:].iloc[:, 0].tolist())
-
-training_labs = label_data[:30000].iloc[:, 0].tolist()
-valid_labs    = label_data[30000:40001].iloc[:, 0].tolist()
-
-# Get the vocabulary
-vocabulary = create_vocab(imdb_data)
-
-# set an alpha for Laplace
-alpha = 1
-
-# Set up the two lists we'll be filling in this for loop
-likelyhoods = []
-mylist = [[],[]]
-prob = []
-
-print('starting part1')
-# Go through each review and add each word to a list depending if the review is positive of negative
-for i, lab in enumerate(set(training_labs)):
-    prob.append(math.log(training_labs.count(lab) / len(training_labs)))
+# Function to propogate the two wordcount lists for each label
+def getLabs():
+    wcList = [[],[]]
     for j, elem in enumerate(training_labs):
         line = training_data[j]
         # Only two classes here, so we can shortcut
         if(elem == "positive"):
             for word in line:
                 if(word in vocabulary):
-                    mylist[1].append(word)
+                    wcList[1].append(word)
         else:
             for word in line:
                 if(word in vocabulary):
-                    mylist[0].append(word)
+                    wcList[0].append(word)
+    return wcList
 
-    print('total_words')
-    # Get the total number of words from the vocabulary
-    total_words = 0
-    for word in vocabulary:
-        total_words += mylist[i].count(word)
 
-    print('likelyhoods')
-    # Compute the likelyhoods of each word in the vocabulary
-    likelyhoods.clear()
-    for word in vocabulary:
-        count = mylist.count(word)
-        likelyhoods.append(math.log((count + alpha) / (total_words + alpha * len(vocabulary))))
+def getLikelyhoods(alpha):
+    likelyhoods = [[],[]]
+    prob = []
+    # Loop through each label (pos and neg) and get the probability, the total words, and the likelyhoods for each word seen in that label
+    for i, lab in enumerate(set(training_labs)):
+        prob.append(math.log(training_labs.count(lab) / len(training_labs)))
 
-# For each review, grab the likelyhood that word is in a positive review and sum them
-# Not sure on this part, definitely need to understand this better
-i = -1
-correct = 0
-pred_list = []
+        # Get the total number of words from the vocabulary
+        total_words = 0
+        for word in vocabulary:
+            total_words += wcList[i].count(word)
 
-print('starting part 2')
-for line in valid_data:
-    i += 1
-    sums = [0]*2
-    for j, lab in enumerate(set(training_labs)):
-        sums[j] = prob[j]
-        for word in line:
-            if word in vocabulary:
-                sums[j] += likelyhoods[likelyhoods.index(word)]
-    
-    pred = -1
-    if(sums[0] >= sums[1]):
-        pred = "negative"
-    else:
-        pred = "positive"
+        # Compute the likelyhoods of each word in the vocabulary
+        for word in vocabulary:
+            count = wcList[i].count(word)
+            likelyhoods[i].append(math.log((count + alpha) / (total_words + alpha * len(vocabulary))))
+    return likelyhoods, prob
 
-    if(pred == valid_labs[i]):
-        correct += 1
-    pred_list.append(pred)
-    
-end = time.time()
-print("Percent Correct: " + str(correct/len(valid_labs)) + "\n\n")
-print("time taken: {}".format(end - start))
-print(pred_list)
+
+def classify(valid_data, valid_labs, likelyhoods):
+    i = -1
+    correct = 0
+    pred_list = []
+
+    for line in valid_data:
+        i += 1
+        sums = [0,0]
+        for j, lab in enumerate(set(valid_labs)):
+            sums[j] = prob[j]
+            for word in line:
+                if word in vocabulary:
+                    sums[j] += likelyhoods[j][vocabulary.index(word)]
+        
+        pred = -1
+        if(sums[0] >= sums[1]):
+            pred = "negative"
+        else:
+            pred = "positive"
+
+        if(pred == valid_labs[i]):
+            correct += 1
+        pred_list.append(pred)
+    return pred_list, correct
+
+
+# Importing the dataset and creating the three distinct datasets
+imdb_data  = pd.read_csv('IMDB.csv', delimiter=',')
+label_data = pd.read_csv('IMDB_labels.csv', delimiter=',') 
+
+# Get the vocabulary
+vocabulary = create_vocab(imdb_data)
+
+# Separate out the three data sets and clean them using the preprocessor
+training_data = create_list(imdb_data[:30000].iloc[:, 0].tolist())
+valid_data    = create_list(imdb_data[30000:40000].iloc[:, 0].tolist())
+testing_data  = create_list(imdb_data[40000:].iloc[:, 0].tolist())
+
+# Break out the validation labels from the training labels
+training_labs = label_data[:30000].iloc[:, 0].tolist()
+valid_labs    = label_data[30000:40000].iloc[:, 0].tolist()
+
+# set an alpha for Laplace
+alpha = 1
+
+# Set up the three lists we'll be using for classification
+wcList = getLabs()
+likelyhoods, prob = getLikelyhoods(alpha)
+
+# Run the classification on the validation data
+pred_list, correct = classify(valid_data, valid_labs, likelyhoods)
+
+# Print out the percent correct on the validation set
+print("Percent Correct: " + str(round(correct/len(valid_labs)*100),2))
+
+# Print the list of predictions to an output file
+with open('test_predicitons.csv', 'w') as outfile:
+    for line in pred_list:
+        outfile.write(line + "\n")
