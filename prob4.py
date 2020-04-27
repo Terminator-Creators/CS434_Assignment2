8dffc6fe9bcef3be90308f0674c9cf7ce73fd213
jtparks <parksju@oregonstate.edu>
2020-04-26 19:55:50 -0700

    added full problem4 support

--- prob4.py
+++ prob4.py
@@ -3,7 +3,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 import re
 import math
 import time
+import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Function to make a 1D list of sentences into a 2D list of words
 def create_list(aList):
@@ -38,7 +40,7 @@ def clean_text(text):
 
 
 # Function to create the vocab from the imdb_data
-def create_vocab(imdb_data, alpha):
+def create_vocab(imdb_data, alpha, vec_lwr, vec_upr):
     # this vectorizer will skip stop words
     vectorizer = CountVectorizer(
         stop_words="english",
@@ -78,7 +80,10 @@ def create_vocab(imdb_data, alpha):
     i = 0
     correct = 0
     pred_list = []
-    vector_validate = np.copy(vector[30000:40000])
+    # Change the vector limits to pull different sets, 
+    # 30000:40000 = Validation
+    # 40000:50000 = Testing
+    vector_validate = np.copy(vector[vec_lwr:vec_upr])
     for item in vector_validate:
         spam_value = (item.T*spam_likelyhood).T.sum() + prob[0]
         non_spam_value = (item.T*non_spam_likelyhood).T.sum() + prob[1]
@@ -87,8 +92,10 @@ def create_vocab(imdb_data, alpha):
             pred = "positive"
         else:
             pred = "negative"
-        if(pred == valid_labs[i]):
-            correct += 1
+        
+        if(vec_lwr == 30000):
+            if(pred == valid_labs[i]):
+                correct += 1
         pred_list.append(pred)
         i += 1
 
@@ -109,16 +116,31 @@ training_labs = label_data[:30000].iloc[:, 0].tolist()
 valid_labs    = label_data[30000:40000].iloc[:, 0].tolist()
 
 # set an alpha for Laplace
-alpha = 1
+alphas = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
 prob = [np.log(training_labs.count("negative") / len(training_labs)), np.log(training_labs.count("positive") / len(training_labs))]
+accuracy_list = []
 
-# Get the vocabulary and two lists of ints of word occurances plus the likelyhoods
-pred_list, correct = create_vocab(imdb_data, alpha)
+for val in alphas:
+    # Get the vocabulary and two lists of ints of word occurances plus the likelyhoods
+    pred_list, correct = create_vocab(imdb_data, val, 30000, 40000)
+    accuracy_list.append(round(correct/len(valid_labs)*100,4))
+    # Print out the percent correct on the validation set
+    print("Percent Correct: " + str(round(correct/len(valid_labs)*100,4)))
+    
+plt.rcParams['font.family'] = ['serif']
+plt.plot(alphas, accuracy_list)
+plt.xlabel("Alpha Values")
+plt.xticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0])
+plt.ylabel("Validation Accuracy")
+plt.savefig("alphas_vs_acc.png")
+plt.clf()
 
-# Print out the percent correct on the validation set
-print("Percent Correct: " + str(round(correct/len(valid_labs)*100,2)))
+# Run the alpha = 1.0 version to get the predictions 
+pred_list, correct = create_vocab(imdb_data, 1.0, 40000, 50000)
 
 # Print the list of predictions to an output file
-with open('test_predicitons.csv', 'w') as outfile:
+with open('test_predictions2.csv', 'w') as outfile:
     for line in pred_list:
-        outfile.write(line + "\n")
+        outfile.write(str(line))
+        if(not(line == pred_list[:-1])):
+            outfile.write("\n")
